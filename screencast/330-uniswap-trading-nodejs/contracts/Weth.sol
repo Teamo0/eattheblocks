c14c70c23b5a2e6b11369365431ef8d7e3b68a5e
Teamo0 <77360332+Teamo0@users.noreply.github.com>
2022-01-14 00:09:46 +0000

    update tokenswap

--- screencast/330-uniswap-trading-nodejs/contracts/Weth.sol
+++ screencast/330-uniswap-trading-nodejs/contracts/Weth.sol
@@ -5,4 +5,5 @@ contract Weth {
   function approve(address spender, uint amount) external {}
   function allowance(address owner, address spender) external view returns(uint) {}
   function balanceOf(address owner) external view returns(uint) {}
+  function withdraw(uint wad) public {}
 }
--- screencast/330-uniswap-trading-nodejs/scripts/trade.js
+++ screencast/330-uniswap-trading-nodejs/scripts/trade.js
@@ -1,42 +1,65 @@
-const Router = artifacts.require('Router.sol');
-const Weth = artifacts.require('Weth.sol');
-const Dai = artifacts.require('Dai.sol');
+const ethers = require('ethers');
+onst hre = require("hardhat")
+
+require('dotenv').config();
+
+const router_artifacts = hre.artifacts.readArtifact("Router");
+const dai_artifacts = hre.artifacts.readArtifact("Dai");
+const weth_artifacts = hre.artifacts.readArtifact("Weth");
+
+
+// const Router = artifacts.require('Router.sol');
+// const Weth = artifacts.require('Weth.sol');
+// const Dai = artifacts.require('Dai.sol');
 
 const ROUTER_ADDRESS = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D';
 const WETH_ADDRESS = '0xc778417E063141139Fce010982780140Aa0cD5Ab';
 const DAI_ADDRESS = '0x5592EC0cfb4dbc12D3aB100b257153436a1f0FEa'; 
 
-const amountIn = web3.utils.toWei('0.1');
+const amountIn = ethers.utils.parseUnits('0.1', 'wei');
+
+const url = process.env.RINKEBY_URL;
+const provider = new ethers.providers.JsonRpcProvider(url);
+
+let privateKey = process.env.PRIVATE_KEY;
+let wallet = new ethers.Wallet(privateKey, provider);
+
+
 
 module.exports = async done => {
   try {
-    const [admin, _] = await web3.eth.getAccounts();
+    // const [admin, _] = await web3.eth.getAccounts();
+    // const router = await Router.at(ROUTER_ADDRESS);
+    // const weth = await Weth.at(WETH_ADDRESS);
+    // const dai = await Dai.at(DAI_ADDRESS);
+
+    //const [admin, _] = await web3.eth.getAccounts();
     const router = await Router.at(ROUTER_ADDRESS);
     const weth = await Weth.at(WETH_ADDRESS);
     const dai = await Dai.at(DAI_ADDRESS);
 
-    await weth.deposit({value: amountIn}) 
-    await weth.approve(router.address, amountIn);
+  //   await weth.deposit({value: amountIn}) 
+  //   await weth.approve(router.address, amountIn);
 
-    const amountsOut = await router.getAmountsOut(amountIn, [WETH_ADDRESS, DAI_ADDRESS]);
-    const amountOutMin = amountsOut[1]
-        .mul(web3.utils.toBN(90))
-        .div(web3.utils.toBN(100));
-    const balanceDaiBefore = await dai.balanceOf(admin);
+  //   const amountsOut = await router.getAmountsOut(amountIn, [WETH_ADDRESS, DAI_ADDRESS]);
+  //   const amountOutMin = amountsOut[1]
+  //       .mul(web3.utils.toBN(90))
+  //       .div(web3.utils.toBN(100));
+  //   const balanceDaiBefore = await dai.balanceOf(admin);
 
-    await router.swapExactTokensForTokens(
-      amountIn, 
-      amountOutMin,
-      [WETH_ADDRESS, DAI_ADDRESS],
-      admin,
-      Math.floor((Date.now() / 1000)) + 60 * 10
-    );
+  //   await router.swapExactTokensForTokens(
+  //     amountIn, 
+  //     amountOutMin,
+  //     [WETH_ADDRESS, DAI_ADDRESS],
+  //     admin,
+  //     Math.floor((Date.now() / 1000)) + 60 * 10
+  //   );
 
-    const balanceDaiAfter = await dai.balanceOf(admin);
-    const executionPerf = balanceDaiAfter.sub(balanceDaiBefore).div(amountsOut[1]);
-    console.log(executionPerf.toString());
-  } catch(e) {
-    console.log(e);
-  }
-  done();
+  //   const balanceDaiAfter = await dai.balanceOf(admin);
+  //   const executionPerf = balanceDaiAfter.sub(balanceDaiBefore).div(amountsOut[1]);
+  //   console.log(executionPerf.toString());
+  // } catch(e) {
+  //   console.log(e);
+  // }
+  // done();
 };
